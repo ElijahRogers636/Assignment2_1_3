83c0e1dfe7200f716207582cde5bea06d95ce396
Elijah Rogers <ejrogersemail@gmail.com>
2024-11-11 13:25:34 -0500

    Add project files.

---
+++ Maths.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Assignment2_1_3
+{
+    //Overloading: Write a simple Maths class ( don’t use the keyword Math, it will be a conflict with standard class from system).
+    //Write overloaded methods with logic and give choice to user to call different methods :
+    //a.Add(int num1, int num2)
+    //b.Add(decimal num1, decimal num2, decimal num3)
+    //c.Multiply(float num1, float num2)
+    //d.Multiply(float num1, float num2, float num3)
+    //Declare these methods as public and static.
+
+    public class Maths
+	{
+        public Maths() { }
+		
+        //Method for user selection with hardcoded numbers
+        public void UserSelection(string selection)
+        {
+            switch (selection)
+            {
+                case "1":
+                    Console.WriteLine(Add(5, 6));
+                    break;
+                case "2":
+                    Console.WriteLine(Add(2, 3, 5));
+                    break;
+                case "3":
+                    Console.WriteLine(Multiply(2, 8));
+                    break;
+                case "4":
+                    Console.WriteLine(Multiply(2, 4, 8));
+                    break;
+                default:
+
+                    break;
+            }
+        }
+
+        //Overloaded Methods
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static decimal Add(decimal num1, decimal num2, decimal num3)
+        {
+            return num1 + num2 + num3;
+        }
+
+        public static float Multiply(float num1, float num2)
+        {
+            return num1 * num2;
+        }
+
+        public static float Multiply(float num1, float num2, float num3)
+        {
+            return num1 * num2 * num3;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Assignment2_1_3
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Maths maths = new Maths();
+
+            Console.WriteLine("Which method would you like to use");
+            Console.WriteLine("(1) Add(int num1, int num2)");
+            Console.WriteLine("(2) Add(decimal num1, decimal num2, decimal num3");
+            Console.WriteLine("(3) Multiply(float num1, float num2)");
+            Console.WriteLine("(4) Multiply(float num1, float num2, float num3)");
+            Console.Write("Choice: ");
+            string selection = Console.ReadLine();
+
+            maths.UserSelection(selection);
+
+        }
+    }
+}
